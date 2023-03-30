5f66c210207b7027322658860a9a3176c5de8cb4
Anton <anton.sjolund@outlook.com>
2023-03-30 10:24:06 +0200

    test

---
+++ Day1-Variables/input.py
@@ -0,0 +1,5 @@
+# putting input into the print function.
+# print("Hello " + input("What is your name? ") + "!")
+
+#Write your code below this line 👇
+print(len(input("What is your name? ")))
---
+++ Day1-Variables/printing.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')\n")
+
+print("Hello world!\nHello world!") # \n same as new line.
+
+
+
+
+
+
